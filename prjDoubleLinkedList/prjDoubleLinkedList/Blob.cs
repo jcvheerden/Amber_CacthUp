efcd0ec071fa34449ad699920e1abeef7dff3c64
Amber-222 <ST10287325@vcconnect.edu.za>
2025-07-24 11:49:35 +0200

    new prog stuff
    did another activity with reece and homework sections were given

---
+++ prjDoubleLinkedList/prjDoubleLinkedList/Blob.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjDoubleLinkedList
+{
+    public class Blob
+    {
+        public Node root;
+
+        //checks if the node exists, whats previous and next nodes
+        public void add(int number)
+        {
+            if (number == null)
+            {
+                return;
+            }
+            else if (root == null)
+            {
+                root = new Node(number);
+            }
+            else
+            {
+                Node n = new Node(number);
+                Node current = root;
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                n.previous = current;
+                current.next = n;
+            }
+        }
+    }
+}
---
+++ prjDoubleLinkedList/prjDoubleLinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace prjDoubleLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Blob blob = new Blob();
+            blob.add(6);
+            blob.add(5);
+            blob.add(4);
+            blob.add(3);
+            blob.add(2);
+            blob.add(1);
+            blob.add(0);
+
+            Node root = blob.root;
+            Node newNode = root.next.next.next.next; //separate single objectsm, can loop through them all
+            Console.WriteLine("Head node value: " + root.value);
+            Console.WriteLine("New node value: " + newNode.value);
+            Console.WriteLine("Total nodes: " + countNodes(root));
+        }
+
+        public static int countNodes(Node root)
+        {
+            int count = 1;
+            Node current = root;
+            while (current.next != null)
+            {
+                current = current.next;
+                count++;
+            }
+
+            return count;
+        }
+
+        /*HOMEWORK FOR TOMORROW*/
+        public static Node getLast(Node root)
+        {
+
+        }
+
+        public static Node getFirst(Node middle)
+        {
+
+        }
+
+        //find inside the list and indicate that spot and show the full list of all nodes
+        //find first spot of thTat node and print out the entire list that comes after
+        public static void findNodePrintAll(Node current)
+        {
+
+        }
+    }
+}
